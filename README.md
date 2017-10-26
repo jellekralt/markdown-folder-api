a9d2ed1bb44ae8b45e37a6bad9450264c32dd38f
Jelle Kralt <jelle@jellekralt.nl>
2017-10-26 13:28:00 +0200

    Added tests

--- README.md
+++ README.md
@@ -3,4 +3,13 @@ Markdown Folder API
 
 Takes a folder path as an imput, and parses the markdown files inside. The parsed files are stored and can be retreived with an API.
 
+## Install
+```bash
+# NPM
+npm i markdown-folder-api 
+
+# Yarn
+yarn add markdown-folder-api
+```
+
 ***This software is currently BETA quality, use at your own risk***
--- test/index.js
+++ test/index.js
@@ -1,16 +1,60 @@
+const chai = require('chai');
+const should = require('chai').should();
 const MarkdownFolder = require('../src/MarkdownFolder');
 const { resolve } = require('path');
 
-let mdFiles = new MarkdownFolder({
-  path: resolve(__dirname, 'mocks/md-files')
+chai.should();
+
+describe('MarkdownFolder', function() {
+  let mdFiles;
+
+  beforeEach(function() {
+    mdFiles = new MarkdownFolder({
+      path: resolve(__dirname, 'mocks/md-files')
+    });
+  });
+
+  describe('#create', function() {
+    it('should load', function(done) {
+      mdFiles.on('load', () => {
+        done();
+      });
+    });
+  });
+
+  describe('#getAll', function() {
+    it('should return all posts', function(done) {
+      mdFiles.on('load', () => {
+        Object.keys(mdFiles.getAll()).should.have.length(3);
+        done();
+      });
+    });
+  });
+
+  describe('#get', function() {
+    it('should return a single post', function(done) {
+      mdFiles.on('load', () => {
+        let post = mdFiles.get('bar');
+
+        post.should.have.property('filePath');
+        done();
+      });
+    });
+
+    it('should return undefined when non found', function(done) {
+      mdFiles.on('load', () => {
+        let post = mdFiles.get('notfound');
+
+        should.not.exist(post);
+        done();
+      });
+    });
+  });
 });
 
-mdFiles.on('load', () => {
-  console.log('loaded');
+
+
   
-  console.log(': GET ALL -------------');  
-  console.log(mdFiles.getAll());
-  
-  console.log(': GET bar -------------');  
-  console.log(mdFiles.get('bar'));
-});
+
+
+
